c2957bdbe370179dc5a807675f0686a6c396faac
Alexandr <vorblex@gmail.com>
2017-09-28 22:00:08 +0300

    add header and about section

--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -5,7 +5,7 @@ module.exports = function () {
   $.gulp.task('sprite', () => {
     let spriteData = $.gulp.src('./source/icons/*.png').pipe($.gp.spritesmith({
       imgName: 'sprite.png',
-      cssName: 'sprite.css',
+      cssName: 'sprite.scss',
       imgPath: '/assets/img/sprite.png',
       cssFormat: 'css',
       algorithm: 'left-right',
@@ -15,7 +15,7 @@ module.exports = function () {
       .pipe($.gulp.dest('./source/images')); // путь куда записываем спрайт
 
     let cssStream = spriteData.css
-      .pipe($.gulp.dest('./source/style/common')); // путь куда записываем файл стилей для спрайта
+      .pipe($.gulp.dest('./source/styles/_misc')); // путь куда записываем файл стилей для спрайта
 
     return $.merge(imgStream, cssStream);
   });
--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ global.$ = {
     task: require('./gulp/paths/tasks.js'),
     jsFoundation: require('./gulp/paths/js.foundation.js'),
     cssFoundation: require('./gulp/paths/css.foundation.js'),
-    app: require('./gulp/paths/app.js'),
+    app: require('./gulp/paths/app.js')
   },
   gulp: require('gulp'),
   del: require('del'),
@@ -19,9 +19,9 @@ global.$ = {
   fs: require('fs'),
   gp: require('gulp-load-plugins')({
     rename: {
-      'gulp-replace-task': 'replaceTask',
-    },
-  }),
+      'gulp-replace-task': 'replaceTask'
+    }
+  })
 };
 
 $.path.task.forEach((taskPath) => {
